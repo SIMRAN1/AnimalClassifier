48a0e0a136ebd0299331d0726b9ab5aa9d9fe090
SIMRAN1 <simranpreetkaurkhalsa13@gmail.com>
2019-01-07 16:14:56 +0530

    Adding Photo Picker action

--- AnimalClassifier/controller/AnimalClassificationViewController.swift
+++ AnimalClassifier/controller/AnimalClassificationViewController.swift
@@ -16,11 +16,47 @@ class AnimalClassificationViewController: UIViewController {
     @IBOutlet weak var classificationLbl: UILabel!
     @IBOutlet weak var cameraBtn: UIBarButtonItem!
     
-   
+    override func viewDidLoad() {
+        
+    }
     
     @IBAction func cameraBtnWasPressed(_ sender: Any) {
         
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            presentPhotoPicker(sourceType: .photoLibrary)
+            return
+        }
+        
+        let photoSourcePicker = UIAlertController()
+        let takePhotoAction = UIAlertAction(title: "Take Photo", style: .default) { _ in
+            self.presentPhotoPicker(sourceType: .camera)
+        }
+        
+        let choosePhotoAction = UIAlertAction(title: "Choose Photo", style: .default) { _ in
+            self.presentPhotoPicker(sourceType: .photoLibrary)
+        }
+        photoSourcePicker.addAction(takePhotoAction)
+        photoSourcePicker.addAction(choosePhotoAction)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        photoSourcePicker.addAction(cancelAction)
+        present(photoSourcePicker, animated: true, completion: nil)
 }
 
+    func presentPhotoPicker(sourceType : UIImagePickerController.SourceType) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = sourceType
+        present(picker, animated: true, completion: nil)
+    }
 
+}
+
+extension AnimalClassificationViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            fatalError("No Image Returned")
+        }
+        imageView.image = image
+    }
 }
