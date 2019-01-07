1c0f14edaa85ed8f0eabc4dd725d732341e3f58e
SIMRAN1 <simranpreetkaurkhalsa13@gmail.com>
2019-01-07 15:40:14 +0530

    Reslving error could not create outlet

---
+++ AnimalClassifier/controller/AnimalClassificationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MyZoo
+//
+//  Created by Caleb Stultz on 8/3/18.
+//  Copyright © 2018 Caleb Stultz. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class AnimalClassificationViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var classificationLbl: UILabel!
+    @IBOutlet weak var cameraBtn: UIBarButtonItem!
+    
+   
+    
+    @IBAction func cameraBtnWasPressed(_ sender: Any) {
+        
+}
+
+
+}
